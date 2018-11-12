ce3cd5a9d6b97fd68af64f1636201b0f68571426
cm <43211374@qq.com>
2018-11-12 09:30:28 +0000

    更新个人资料

---
+++ resources/views/layouts/_message.blade.php
@@ -0,0 +1,26 @@
+@if(Session::has('mession'))
+
+    <div>
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        {{ Session::get('message') }}
+    </div>
+
+@endif
+
+@if(Session::has('success'))
+
+    <div class="alert alert-success">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        {{ Session::get('success') }}
+    </div>
+
+@endif
+
+@if (Session::has('danger'))
+
+    <div class="alert alert-danger">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        {{ Session::get('danger') }}
+    </div>
+
+@endif
--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,6 @@ Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::resource('users','UserController',['only'=>['show','update','edit']]);

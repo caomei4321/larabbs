84681343c65d36500df9661ac14e71de2f4b870c
cm <43211374@qq.com>
2018-11-16 09:32:14 +0000

    update user avatar

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,23 +5,30 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
+use App\Handlers\ImageUploadHandler;
 
 class UserController extends Controller
 {
     public function show(User $user)
     {
-        return view('users.show',compact('user'));
+        return view('users.show', compact('user'));
     }
 
     public function edit(User $user)
     {
-        return view('users.edit',compact('user'));
+        return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request,User $user)
+    public function update(UserRequest $request, User $user, ImageUploadHandler $uploader)
     {
+        $data = $request->all();
+        if ($request->avatar) {
+            $result = $uploader->save($request->avatar, 'avatars', $user->id,362);
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
         $user->update($request->all());
-
-        return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功');
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功');
     }
 }

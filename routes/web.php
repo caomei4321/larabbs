600dad12e579111ece75cdbe0368b48842f4cd93
cm <43211374@qq.com>
2018-12-29 05:41:36 +0000

    富文本文件上传

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,5 @@ Route::resource('users', 'UserController', ['only' => ['show', 'update', 'edit']
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+
+Route::post('upload_image','TopicsController@upload_image')->name('topics.upload_image');
